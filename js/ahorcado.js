082c24f0e11035bcab08eedaf8e92df8bd05757c
FacuCR <facucastro302@gmail.com>
2022-05-25 20:29:47 -0300

    dibujo del humano listo

--- js/ahorcado.js
+++ js/ahorcado.js
@@ -4,6 +4,8 @@ let basePosition = { x: 50, y: 300 };
 let columnPosition = { x: 90, y: 50 };
 let supportPosition = { x: 100, y: 50 };
 let ropePosition = { x: 180, y: 50 };
+let legsPosition = { x: 185, y: 250};
+let armsPosition = { x: 185, y: 170};
 
 let canvas = document.getElementById("ahorcado");
 if (canvas.getContext) {
@@ -16,6 +18,12 @@ if (canvas.getContext) {
     gallowsColor,
     ctx
   );
+  headDrawing(humanColor, ctx);
+  bodyDrawing(182, 150, humanColor, ctx);
+  leftLegDrawing(legsPosition.x, legsPosition.y, humanColor, ctx);
+  rightLegDrawing(legsPosition.x, legsPosition.y, humanColor, ctx);
+  leftArmDrawing(armsPosition.x, armsPosition.y, humanColor, ctx);
+  rightArmDrawing(armsPosition.x, armsPosition.y, humanColor, ctx)
 }
 
 function baseDrawing(x, y, color, canvasContext) {
@@ -60,4 +68,59 @@ function gallowsDrawing(
     canvasContext
   );
   ropeDrawing(ropePosition.x, ropePosition.y, gallowsColor, canvasContext);
+}
+
+function headDrawing(color, canvasContext) {
+  canvasContext.beginPath();
+  canvasContext.lineWidth = "3";
+  canvasContext.strokeStyle = color;
+  canvasContext.arc(185, 120, 30, 0, Math.PI * 2, true); // Outer circle
+  canvasContext.moveTo(201.5, 135);
+  canvasContext.arc(185, 135, 18, 0, Math.PI, true); // Mouth (clockwise)
+  canvasContext.moveTo(180, 110);
+  canvasContext.arc(175, 110, 5, 0, Math.PI * 2, true); // Left eye
+  canvasContext.moveTo(200, 110);
+  canvasContext.arc(195, 110, 5, 0, Math.PI * 2, true); // Right eye*/
+  canvasContext.stroke();
+}
+
+function bodyDrawing(x, y, color, canvasContext) {
+  canvasContext.fillStyle = color;
+  canvasContext.fillRect(x, y, 5, 100);
+}
+
+function leftLegDrawing(x, y, color, canvasContext) {
+    canvasContext.beginPath();
+    canvasContext.lineWidth = "3";
+    canvasContext.strokeStyle = color;
+    canvasContext.moveTo(x, y);
+    canvasContext.lineTo(x + 25, y + 30);
+    canvasContext.stroke();
+}
+
+function rightLegDrawing(x, y, color, canvasContext) {
+  canvasContext.beginPath();
+  canvasContext.lineWidth = "3";
+  canvasContext.strokeStyle = color;
+  canvasContext.moveTo(x, y);
+  canvasContext.lineTo(x - 25, y + 30);
+  canvasContext.stroke();
+}
+
+function leftArmDrawing(x, y, color, canvasContext) {
+    canvasContext.beginPath();
+    canvasContext.lineWidth = "3";
+    canvasContext.strokeStyle = color;
+    canvasContext.moveTo(x, y);
+    canvasContext.lineTo(x + 25, y + 30);
+    canvasContext.stroke();
+}
+
+function rightArmDrawing(x, y, color, canvasContext) {
+  canvasContext.beginPath();
+  canvasContext.lineWidth = "3";
+  canvasContext.strokeStyle = color;
+  canvasContext.moveTo(x, y);
+  canvasContext.lineTo(x - 25, y + 30);
+  canvasContext.stroke();
 }
