77a5edb34aa02afa89d47b86121bd702587027d8
FacuCR <facucastro302@gmail.com>
2022-05-26 00:50:35 -0300

    dibujar brazos, control de logica de ingreso de letras y palabra secreta listos

--- js/ahorcado.js
+++ js/ahorcado.js
@@ -4,8 +4,27 @@ let basePosition = { x: 50, y: 300 };
 let columnPosition = { x: 90, y: 50 };
 let supportPosition = { x: 100, y: 50 };
 let ropePosition = { x: 180, y: 50 };
-let legsPosition = { x: 185, y: 250};
-let armsPosition = { x: 185, y: 170};
+let legsPosition = { x: 185, y: 250 };
+let armsPosition = { x: 185, y: 170 };
+
+const inputWord = document.querySelector(".input-word");
+let secretWordElement = document.querySelector(".secret-word");
+
+let testWord = "SECRETA";
+let secretTestWord = "";
+
+for (let i = 0; i < testWord.length; i++) {
+  secretTestWord += "-";
+}
+modifySecretWord(secretTestWord);
+
+inputWord.addEventListener("keypress", (event) => {
+  if (event.key == "Enter") {
+    let newSecretWord = checkLetter();
+    modifySecretWord(newSecretWord);
+    secretTestWord = newSecretWord;
+  }
+});
 
 let canvas = document.getElementById("ahorcado");
 if (canvas.getContext) {
@@ -23,7 +42,7 @@ if (canvas.getContext) {
   leftLegDrawing(legsPosition.x, legsPosition.y, humanColor, ctx);
   rightLegDrawing(legsPosition.x, legsPosition.y, humanColor, ctx);
   leftArmDrawing(armsPosition.x, armsPosition.y, humanColor, ctx);
-  rightArmDrawing(armsPosition.x, armsPosition.y, humanColor, ctx)
+  rightArmDrawing(armsPosition.x, armsPosition.y, humanColor, ctx);
 }
 
 function baseDrawing(x, y, color, canvasContext) {
@@ -90,12 +109,12 @@ function bodyDrawing(x, y, color, canvasContext) {
 }
 
 function leftLegDrawing(x, y, color, canvasContext) {
-    canvasContext.beginPath();
-    canvasContext.lineWidth = "3";
-    canvasContext.strokeStyle = color;
-    canvasContext.moveTo(x, y);
-    canvasContext.lineTo(x + 25, y + 30);
-    canvasContext.stroke();
+  canvasContext.beginPath();
+  canvasContext.lineWidth = "3";
+  canvasContext.strokeStyle = color;
+  canvasContext.moveTo(x, y);
+  canvasContext.lineTo(x + 25, y + 30);
+  canvasContext.stroke();
 }
 
 function rightLegDrawing(x, y, color, canvasContext) {
@@ -108,12 +127,12 @@ function rightLegDrawing(x, y, color, canvasContext) {
 }
 
 function leftArmDrawing(x, y, color, canvasContext) {
-    canvasContext.beginPath();
-    canvasContext.lineWidth = "3";
-    canvasContext.strokeStyle = color;
-    canvasContext.moveTo(x, y);
-    canvasContext.lineTo(x + 25, y + 30);
-    canvasContext.stroke();
+  canvasContext.beginPath();
+  canvasContext.lineWidth = "3";
+  canvasContext.strokeStyle = color;
+  canvasContext.moveTo(x, y);
+  canvasContext.lineTo(x + 25, y + 30);
+  canvasContext.stroke();
 }
 
 function rightArmDrawing(x, y, color, canvasContext) {
@@ -123,4 +142,25 @@ function rightArmDrawing(x, y, color, canvasContext) {
   canvasContext.moveTo(x, y);
   canvasContext.lineTo(x - 25, y + 30);
   canvasContext.stroke();
+}
+
+function modifySecretWord(word) {
+  secretWordElement.innerHTML = "";
+  let textP = document.createTextNode(word);
+  secretWordElement.append(textP);
+}
+
+function checkLetter() {
+  let inputLetter = inputWord.value.toUpperCase();
+  let newSecretWord = "";
+  for (let i = 0; i < testWord.length; i++) {
+    if (testWord[i] == inputLetter) {
+      newSecretWord += inputLetter;
+    } else if (secretTestWord[i] != "-") {
+      newSecretWord += secretTestWord[i];
+    } else {
+      newSecretWord += "-";
+    }
+  }
+  return newSecretWord;
 }
